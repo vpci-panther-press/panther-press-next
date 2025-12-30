688543f973f46930f010fd52df0a109677e38be8
Surya <tallapaneni.surya@gmail.com>
2025-12-30 17:53:45 -0500

    feat: pre-commit hooks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,25 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import typescript from "typescript-eslint";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
-
-export default eslintConfig;
+export default typescript.config(
+	js.configs.recommended,
+	...typescript.configs.recommended,
+	{
+		languageOptions: {
+			parserOptions: {
+				ecmaVersion: "latest",
+				sourceType: "module",
+				jsx: true,
+			},
+		},
+		rules: {
+			"@typescript-eslint/no-unused-vars": [
+				"error",
+				{
+					argsIgnorePattern: "^_",
+					varsIgnorePattern: "^_",
+				},
+			],
+		},
+	}
+);
