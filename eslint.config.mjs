ea15835e4ace44766f843deddbb73ee90bb0419c
Surya <tallapaneni.surya@gmail.com>
2025-12-30 19:08:58 -0500

    fix: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,24 +2,34 @@ import js from "@eslint/js";
 import typescript from "typescript-eslint";
 
 export default typescript.config(
-	js.configs.recommended,
-	...typescript.configs.recommended,
-	{
-		languageOptions: {
-			parserOptions: {
-				ecmaVersion: "latest",
-				sourceType: "module",
-				jsx: true,
-			},
-		},
-		rules: {
-			"@typescript-eslint/no-unused-vars": [
-				"error",
-				{
-					argsIgnorePattern: "^_",
-					varsIgnorePattern: "^_",
-				},
-			],
-		},
-	}
+  js.configs.recommended,
+  ...typescript.configs.recommended,
+  {
+    ignores: [
+      "dist/**",
+      "node_modules/**",
+      "build/**",
+      ".wrangler/**",
+      ".open-next/**",
+      ".next/**",
+    ],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        jsx: true,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  }
 );
